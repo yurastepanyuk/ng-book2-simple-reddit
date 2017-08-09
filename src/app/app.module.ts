70e08143232fb9592c2360a74e73cc6d1d399c34
yurastepanyuk <yu.stepanyu.ra@gmail.com>
2017-08-09 10:14:46 +0300

    add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
